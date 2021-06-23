b0015b09a1fb08746458cf3e7c81c0bf22c7a680
Andrew Masih <armasih@outlook.com>
2021-06-23 13:56:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/* it depend on what you want to convert, we can converts ARRAYS into STRINGs and NUMBERS into STRINGs and visa versa
+
+'type of' operator tells us the data type of an object.
+
+if you work with some objects in JS, and at somepoint we need to be sure our data is a string, array or something else, to confirm is a number is a number
+*/
+
+let numberOrString = "12";
+
+typeof (numberOrString);
+
+//now we know the data is a string
